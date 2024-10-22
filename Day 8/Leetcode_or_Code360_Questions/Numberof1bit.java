bb196d73b5ab3ba9a2063632b7c067c09a2ec678
Raikant59 <rakantkumar1234@gmail.com>
2024-10-22 23:15:18 +0530

    Day 8

---
+++ Day 8/Leetcode_or_Code360_Questions/Numberof1bit.java
@@ -0,0 +1,18 @@
+// Question Link :- https://leetcode.com/problems/number-of-1-bits/description/
+
+// Solution :- 
+
+/*
+ class Solution {
+    public int hammingWeight(int n) {
+        int count=0;
+        while(n!=0){
+            if ((n&1)==1){
+                count++;
+            }
+            n=n>>1;
+        }
+        return count;
+    }
+ }
+ */
